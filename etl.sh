6496bd3f132fe587de201ad84c43af1ee1307a07
Aneurin Smith <aneurinsmith@yahoo.com>
2025-03-17 18:44:34 +0000

    Edited how data is downloaded by the shell script. Checks if bus_data_wm is already loaded, and if it is, it asks if it should be redownloaded. Deletes the old directories (if they exist) ready for the redownload. Adjusted the cypher-shell commands to execute from where the shell script file is located, so it can be executed from anywhere, not just the base directory of the project.

--- etl.sh
+++ etl.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+CUR_DIR="$(dirname "$0")"
 CSV_DIR="/var/lib/neo4j/import/recce"
-TMP_DIR="/tmp/recce_etl"
+TMP_DIR="/tmp"
+load_data='y'
 
 
 
@@ -8,18 +10,51 @@ TMP_DIR="/tmp/recce_etl"
 # Download Files #
 #----------------#
 
-# Make temporary directories
-echo -e "[ETL] Making temporary directories..."
-echo -e "[ETL] -- $CSV_DIR"
-mkdir $CSV_DIR 2>&1 | awk -e '{printf "\033[0;33m[ETL] -- [ETL] %s\033[0m\n", $0}'
-echo -e "[ETL] -- $TMP_DIR"
-mkdir $TMP_DIR 2>&1 | awk -e '{printf "\033[0;33m[ETL] -- [ETL] %s\033[0m\n", $0}'
+# Detect if they are already installed
+if [ -d "$CSV_DIR/bus_data_wm" ]; then
+    while true; do
+        read -p "[ETL] Bus data already previously loaded. Would you like to reload it? (y/n) " load_data
+        case $load_data in
+            [Yy]* )
+                echo -e "[ETL] Proceeding with reloaded data..."
+                break
+                ;;
+            [Nn]* )
+                echo -e "[ETL] Proceeding without reloading data..."
+                break
+                ;;
+            * )
+                echo -e "\033[0;33m[ETL] Invalid input, please enter y or n\033[0m"
+                ;;
+        esac
+    done
+fi
 
-# Download bus_data
-echo -e "[ETL] Downloading bus_data_wm.zip..."
-# wget -O $TMP_DIR/bus_data_wm.zip 'https://data.bus-data.dft.gov.uk/timetable/download/gtfs-file/west_midlands/'
-echo -e "[ETL] Unzipping bus_data_wm.zip..."
-# unzip $TMP_DIR/bus_data_wm.zip -d $CSV_DIR/bus_data_wm
+# If not installed - or instructed to reload them - install them
+if [ $load_data == 'y' ]; then
+    echo -e "[ETL] Making/clearing temporary directories..."
+    
+    # Create csv directory
+    echo -e "[ETL] -- $CSV_DIR/bus_data_wm"
+    if [ -d "$CSV_DIR/bus_data_wm" ]; then
+        rm -rf "$CSV_DIR/bus_data_wm" 2>/dev/null
+    fi
+    mkdir "$CSV_DIR/bus_data_wm" 2>/dev/null
+
+    # Create tmp directory
+    echo -e "[ETL] -- $TMP_DIR/recce_etl"
+    if [ -d "$TMP_DIR/recce_etl" ]; then
+        rm -rf "$TMP_DIR/recce_etl" 2>&1 | awk -e '{printf "\033[0;33m[ETL] -- [ETL] %s\033[0m\n", $0}'
+    fi
+    mkdir "$TMP_DIR/recce_etl" 2>&1 | awk -e '{printf "\033[0;33m[ETL] -- [ETL] %s\033[0m\n", $0}'
+
+    # Download and unzip files
+    echo -e "[ETL] Downloading bus_data_wm.zip..."
+    wget -O $TMP_DIR/recce_etl/bus_data_wm.zip 'https://data.bus-data.dft.gov.uk/timetable/download/gtfs-file/west_midlands/'
+    
+    echo -e "[ETL] Unzipping bus_data_wm.zip..."
+    unzip $TMP_DIR/recce_etl/bus_data_wm.zip -d $CSV_DIR/bus_data_wm
+fi
 
 
 
@@ -40,36 +75,36 @@ while true; do
     fi
 done
 
-echo -e "[ETL] Ensuring neo4j constraints..."
-cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/create_index.cypher" | 
+echo -e "[ETL] Ensuring neo4j constraints and indexes..."
+cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/create_index.cypher" | 
     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
 
 
 
 echo -e "[ETL] Loading and updating service dates"
-cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/load_calendars.cypher" | 
+cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/load_calendars.cypher" | 
     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
 
 echo -e "[ETL] Loading and updating agencies"
-cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/load_agencies.cypher" | 
+cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/load_agencies.cypher" | 
     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
 
 echo -e "[ETL] Loading and updating routes"
-cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/load_routes.cypher" | 
+cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/load_routes.cypher" | 
     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
 
 echo -e "[ETL] Loading and updating trips"
-cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/load_trips.cypher" | 
+cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/load_trips.cypher" | 
     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
 
 
 
 echo -e "[ETL] Loading and updating stops..."
-cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/load_stops.cypher" | 
+cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/load_stops.cypher" | 
     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
 
 echo -e "[ETL] Assert that each stop has a maximum of 1 parent"
-output=$(cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "_neo4j/assert_parents.cypher" | 
+output=$(cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/assert_parents.cypher" | 
     grep -E 'row' | awk '{print $1}') 
 
 if [[ $output -gt 0 ]]; then
@@ -77,3 +112,9 @@ if [[ $output -gt 0 ]]; then
 else
     echo -e "[ETL] -- All stops validated successfully"
 fi
+
+
+
+# echo -e "[ETL] Loading and updating stoptimes"
+# cypher-shell -a bolt+s://neo4j.aneur.info -u $username -p $password --format verbose < "$CUR_DIR/_neo4j/load_trips.cypher" | 
+#     grep -E 'Added|Created|Set|Deleted' | awk '{print "[ETL] -- " $0}'
