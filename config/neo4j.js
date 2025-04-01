81079b30af681f2462c01c7a663eadde7a74f0fd
Aneurin Smith <aneurinsmith@yahoo.com>
2025-04-01 17:17:28 +0100

    Added database object for interfacing with neo4j database.

--- config/neo4j.js
+++ config/neo4j.js
@@ -1,22 +1,32 @@
 
-const neode = require('/usr/local/lib/node_modules/neode')
+const neo4j = require('/usr/local/lib/node_modules/neo4j-driver');
 
 class Database {
 
-    username = process.env.neo4j_user;
-    password = process.env.neo4j_pass;
-    ip = "neo4j.aneur.info";
-    port = process.env.neo4j_port;
+    USER = process.env.NEO4J_USER;
+    PASS = process.env.NEO4J_PASS;
+    URI = "bolt+s://neo4j.aneur.info";
 
     constructor() {
-        this.instance = new neode(
-            `bolt+s//${this.ip}:${this.port}`,
-            this.username, this.password
+        this.instance = neo4j.driver(this.URI, 
+            neo4j.auth.basic(this.USER, this.PASS),
+            {disableLosslessIntegers: true}
         );
     }
 
-    get_instance() {
-        return this.instance;
+    async execute(query, params = {}) {
+        const {records, summary, keys} = await this.instance.executeQuery(query, params, {database: 'recce'});
+
+        return records.map(record => {
+            return record.keys.reduce((acc, key, index) => {
+                if (record.keys.length > 1) {
+                    acc[key] = record._fields[index];
+                } else {
+                    acc = record._fields[index];
+                }
+                return acc;
+            }, {})
+        })
     }
 }
 
