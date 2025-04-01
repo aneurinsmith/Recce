c551db9a4c6e6fabaaa7bba49ae57c30f6c8f653
Aneurin Smith <aneurinsmith@yahoo.com>
2025-04-01 17:15:24 +0100

    Edited server.js Added ejs view engine Moved 404 handling to seperate function

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 
+const PORT = 7001
+
+const path = require('/usr/local/lib/node_modules/path');
 const dotenv = require('/usr/local/lib/node_modules/dotenv');
 dotenv.config();
 
@@ -7,21 +10,26 @@ const admin = require('./_router/admin');
 const router = require('./_router/router');
 const app = express();
 
-app.use(admin);
-app.use(router);
-app.use('/recce', express.static('_public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+function handle404(req, res) {
+    res.status(404).sendFile('/usr/share/nginx/html/404.html');
+}
 
 app.use((req, res, next)=> {
-    console.log('[TRACE]\t', req.headers['x-forwarded-for'], req.url);
+    console.log('[trace]\t', req.headers['x-forwarded-for'], req.url);
     next();
 });
 
-app.get('*', (req, res)=> {
-    res.status(404).sendFile('/usr/share/nginx/html/404.html');
-});
+app.use(admin);
+app.use(router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/recce', express.static(path.join(__dirname, '_public')));
 
-app.listen(process.env.PORT, ()=> {
-    console.log(' [info]', 'listening on', '217.154.54.14:'+process.env.PORT);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
+
+app.get('*', handle404);
+
+app.listen(PORT, ()=> {
+    console.log(' [info]\t', 'listening on', '217.154.54.14:'+PORT);
 });
