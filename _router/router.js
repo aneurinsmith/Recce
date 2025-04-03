4669d42224d4bac0bac72ce1d75b5c0690eaf0dd
Aneurin Smith <aneurinsmith@yahoo.com>
2025-04-03 23:20:15 +0100

    Altered router to reflect changes made when editing venue_controller

--- _router/router.js
+++ _router/router.js
@@ -4,7 +4,8 @@ const express = require('/usr/local/lib/node_modules/express');
 const router = express.Router();
 
 router.get('/recce/api/:lat,:lng/:venue?', VenueController.get);
-router.get('/recce@:lat,:lng/:venue?', VenueController.index);
-router.get('/recce', VenueController.index);
+router.get('/recce@:lat,:lng/:venue', VenueController.result);
+router.get('/recce@:lat,:lng', VenueController.search);
+router.get('/recce', VenueController.search);
 
 module.exports = router;
